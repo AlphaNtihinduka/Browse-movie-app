da57503748e0a2f06c4ccbc2a10ee8ce351f1e4c
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-24 01:35:24 +0300

    Project init

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    index: './src/js/index.js',
-  },
-  mode: 'development',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-
+    entry: "./src/js/index.js",
+    devServer: {
+        static: "./dist",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
     ],
-  },
-  devServer: {
-    static: './dist',
-  },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }, ],
+    },
 };
