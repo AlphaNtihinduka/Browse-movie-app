1bc8180c3c5252c944a59116c8c78333181ff229
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-24 02:54:15 +0300

    creat basic layout

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,22 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        }, ],
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                use: [{
+                    loader: "html-loader",
+                    options: {
+                        minimize: true,
+                    },
+                }, ],
+            },
+        ],
     },
 };
