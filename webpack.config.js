0bc6273709a7706feefccb7fb39e82db1de80e7e
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-05-24 10:28:35 +0300

    Fix eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/js/index.js",
-    devServer: {
-        static: "./dist",
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-    ],
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        }, ],
-    },
+  entry: './src/js/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }],
+  },
 };
