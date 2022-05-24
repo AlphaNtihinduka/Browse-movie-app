ffd3780c178a7f9df3deba9bf5f985c7f0d0fece
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-24 05:47:34 +0300

    Display movie List

---
+++ config/default.js
@@ -0,0 +1,4 @@
+const _DEFAULT = {
+    _MOVE_API_URL: 'https://api.tvmaze.com/shows',
+}
+export default _DEFAULT;
--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,12 @@
+import { getMovieHandler, renderMovieHandler } from '../../modules/movies.controller';
 import '../css/style.css';
 const movieLists = document.querySelector('.List');
-let _defaultData = [{
-        id: 1,
-        src: "./images/image-equilibrium.jpg",
-        label: "Romance to the Rescue",
-        basicInfo: {
-            year: 2019,
-            time: '90 min',
-            Likes: '10 Likes'
-        }
-    }, {
-        id: 2,
-        src: "./images/image-equilibrium.jpg",
-        label: "Romance to the Rescue",
-        basicInfo: {
-            year: 2015,
-            time: '80 min',
-            Likes: '0 Likes'
-        }
-    },
-    {
-        id: 3,
-        src: "./images/image-equilibrium.jpg",
-        label: "Romance to the Rescue",
-        basicInfo: {
-            year: 1019,
-            time: '1 hr',
-            Likes: '500 Likes'
-        }
-    },
-    {
-        id: 3,
-        src: "./images/image-equilibrium.jpg",
-        label: "Romance to the Rescue",
-        basicInfo: {
-            year: 1019,
-            time: '1 hr',
-            Likes: '500 Likes'
-        }
-    }
-];
-movieLists.innerHTML = _defaultData.map(movie => (
-    ` <div class="card" id=${movie.id}>
-    <img src=${movie.src} alt="" />
-    <div class="contatin">
-        <div class="card-header">
-            <Label>${movie.label}</Label>
+(async() => {
 
-            <span class="material-symbols-outlined">
-          favorite 
-        </span>
-        </div>
-        <ul>
-            <li>
-               ${movie.basicInfo.year}
-            </li>
-            <li>${movie.basicInfo.time}
-            </li>
-            <li>
-                ${movie.basicInfo.Likes}
-            </li>
-        </ul>
-        <button id=${movie.id}>Comment</button>
-        <button id=${movie.id}>Reservation</button>
-    </div>
-</div>`
-));
+    let movieListData = await getMovieHandler();
+    movieListData.length < 0 ?
+        // ? could use spinner component here when fetching data 
+        movieLists.innerHTML = 'loading' :
+        movieLists.innerHTML = renderMovieHandler(movieListData);
+
+})();
