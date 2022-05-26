c896b9fb4d8473f502e0bf36fcd394e0fe7139ee
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-26 20:46:14 +0300

    refacotre code

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,30 +1,30 @@
 import DEFAULT from '../../config/default.js';
 
 const postUrl = `${DEFAULT.INVOLVEMENT_API_BASEURL}/comments`;
-const getUrl = `${DEFAULT.INVOLVEMENT_API_BASEURL}/comments?item_id=1`;
+//!
+const getUrl = `${DEFAULT.INVOLVEMENT_API_BASEURL}/comments?item_id=`;
 
-const commentBtn = document.querySelector('.comment-btn');
 const crossBtn = document.querySelector('.comment-cross');
 const commentPopup = document.querySelector('.comment-popup');
-
+let commentBtn;
 const hidePopup = () => {
-  commentPopup.style.display = 'none';
+    commentPopup.style.display = 'none';
 };
 
 crossBtn.addEventListener('click', hidePopup);
 
-const renderCommentPopup = async (e) => {
-  // const movieId = parseInt(e.target.id.split('_')[1], 10);
-  // const movie = await fetch(`${DEFAULT.MOVE_API_URL}/${movieId}`);
-  const movieCard = e.target.parentElement.parentElement;
-  const cardImageUrl = movieCard.firstElementChild.firstElementChild.src;
-  const releaseDate = movieCard.querySelector('.premiered').innerText;
-  const duration = movieCard.querySelector('.duration').innerText;
-  const name = movieCard.querySelector('.movie-name').innerText;
-  const likes = movieCard.querySelector('.likes').innerText;
+const renderCommentPopup = async(e) => {
+    const movieId = parseInt(e.target.id.split('_')[1], 10);
+    // const movie = await fetch(`${DEFAULT.MOVE_API_URL}/${movieId}`);
+    const movieCard = e.target.parentElement.parentElement;
+    const cardImageUrl = movieCard.firstElementChild.firstElementChild.src;
+    const releaseDate = movieCard.querySelector('.premiered').innerText;
+    const duration = movieCard.querySelector('.duration').innerText;
+    const name = movieCard.querySelector('.movie-name').innerText;
+    const likes = movieCard.querySelector('.likes').innerText;
 
-  let movieDescription = '';
-  movieDescription += `
+    let movieDescription = '';
+    movieDescription += `
       <div class="img-container">
       <img src="${cardImageUrl}" alt="${name}">
       <h1 class="movie-title">${name}</h1>
@@ -39,84 +39,104 @@ const renderCommentPopup = async (e) => {
         <div class="director">👍${likes}</div>
       </div>
     </div>
+    <div class="comment-heading-container"></div>
+    <ul class="comment-holder">
+
+    </ul>
+    <h4 class="add-comment">Add comment</h4>
+    <form action="#" id="comment-form">
+        <input type="text" placeholder="Name" class="comment-inputs name-input">
+        <textarea name="Your insights" id="text-area" cols="30" rows="10" class="comment-inputs comment-text"></textarea>
+        <button type="button" id=${movieId} class="comment-btn">Comment</button>
+    </form>
   `;
-  document.querySelector('.movie-description').innerHTML = movieDescription;
-  commentPopup.style.display = 'block';
+    document.querySelector('.movie-description').innerHTML = movieDescription;
+    commentBtn = document.querySelector('.comment-btn');
+    commentBtn.addEventListener('click', async(e) => {
+        e.preventDefault();
+        const username = document.querySelector('.name-input').value.trim();
+        const comment = document.querySelector('.comment-text').value.trim();
+        let itemId = e.target.id;
+        console.log(itemId, username, comment);
+        if (username !== '' && comment !== '') {
+            await addComment({ itemId, username, comment });
+            insertComment(itemId);
+        }
+        document.getElementById('comment-form').reset();
+    });
+    insertComment(movieId);
+    commentPopup.style.display = 'block';
 };
 
 const commentListener = () => {
-  const homeCommentBtns = document.querySelectorAll('.home-comment-btn');
-  homeCommentBtns.forEach((homeCommentBtn) => {
-    homeCommentBtn.addEventListener('click', renderCommentPopup);
-  });
+    const homeCommentBtns = document.querySelectorAll('.home-comment-btn');
+    console.log(homeCommentBtns);
+    homeCommentBtns.forEach((homeCommentBtn) => {
+        homeCommentBtn.addEventListener('click', renderCommentPopup);
+
+    });
 };
 
-const addComment = async (data) => {
-  const response = await fetch(postUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(
-      {
-        item_id: data.item_id,
-        username: data.username,
-        comment: data.comment,
-      },
-    ),
-  });
-  const commentCall = await response.text();
-  return commentCall;
+// works fine 
+const addComment = async(data) => {
+    const response = await fetch(postUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            item_id: data.itemId,
+            username: data.username,
+            comment: data.comment,
+        }, ),
+    });
+    const commentCall = await response.text();
+    return commentCall;
 };
 
-const getData = async () => {
-  const getResponse = await fetch(getUrl);
-  const result = await getResponse.json();
-  return result;
+const getData = async(id) => {
+    const getResponse = await fetch(`${getUrl}${id}`);
+    const result = await getResponse.json();
+    return result;
 };
 
 const display = (comments) => {
-  let li = '';
-  comments.forEach((commentItem) => {
-    // removed id temporary.
-    li += `
+    let li = '';
+    comments.forEach((commentItem) => {
+        // removed id temporary.
+        li += `
           <li class="comment-detail">
           <span class="date">${commentItem.creation_date}</span>
           <span class="name">${commentItem.username}:</span>
           <span class="commet">${commentItem.comment}</span>
         </li>
     `;
-  });
-  document.querySelector('.comment-holder').innerHTML = li;
+    });
+    document.querySelector('.comment-holder').innerHTML = li;
 };
 
 const index = (indices) => {
-  let sum = 1;
-  for (let i = 1; i < indices.length; i += 1) {
-    sum += 1;
-  }
-  document.querySelector('.comment-heading-container').innerHTML = ` <h3 class="comment-heading">Comments(${sum})</h3> `;
+    let sum = 1;
+    for (let i = 1; i < indices.length; i += 1) {
+        sum += 1;
+    }
+    document.querySelector('.comment-heading-container').innerHTML = ` <h3 class="comment-heading">Comments(${sum})</h3> `;
 };
 
-const insertComment = async () => {
-  const insertDom = await getData();
-  display(insertDom);
-  index(insertDom);
+const insertComment = async(id) => {
+
+    const insertDom = await getData(id);
+    console.log(insertDom);
+    display(insertDom);
+    index(insertDom);
 };
 
-commentBtn.addEventListener('click', async (e) => {
-  e.preventDefault();
-  const username = document.querySelector('.name-input').value.trim();
-  const comment = document.querySelector('.comment-text').value.trim();
-  const itemId = '1';
-  if (username !== '' && comment !== '') {
-    await addComment({ item_id: itemId, username, comment });
-    insertComment();
-  }
-  document.getElementById('comment-form').reset();
-});
+
 
 // export {addComment, getData, hidePopup, renderCommentPopup}
 export {
-  addComment, getData, hidePopup, commentListener,
+    addComment,
+    getData,
+    hidePopup,
+    commentListener,
 };
