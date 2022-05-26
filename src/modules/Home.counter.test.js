783ce880595ec0ab5fed9edc364e2d9af8d5a8fc
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-26 13:41:42 +0300

    Add test function

--- src/modules/Home.counter.test.js
+++ src/modules/Home.counter.test.js
@@ -1,7 +1,44 @@
-const counter = require('./Home.counter');
+import counter from './Home.counter.js'
 
-test('[movie-1,movie-2] should return 2', () => {
+describe("Home page counter method", () => {
+    test('[] should return 0', () => {
+        // TODO: AAA
 
-    expect(counter([1, 2])).toBe(2);
+        //- TODO: ARRANGE
+        let input = [];
 
-})
+        //- TODO: ACT
+        let output = counter(input);
+
+        //- TODO: ASSERT
+        expect(output).toBe(0);
+
+    });
+
+    test('[movie-1,movie-2] should return 2', () => {
+        // TODO: AAA
+
+        //- TODO: ARRANGE
+        let input = ['movie-1', 'movie-2'];
+
+        //- TODO: ACT
+        let output = counter(input);
+
+        //- TODO: ASSERT
+        expect(output).toBe(2);
+
+    })
+    test('[movie-1,movie-2,movie-3,movie-4,movie-5] should return 5', () => {
+        // TODO: AAA
+
+        //- TODO: ARRANGE
+        let input = [];
+
+        //- TODO: ACT
+        let output = counter(input);
+
+        //- TODO: ASSERT
+        expect(output).toBe(0);
+
+    });
+});
