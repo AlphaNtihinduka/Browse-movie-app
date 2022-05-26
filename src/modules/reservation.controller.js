d8ca2664b942c84a06dffb9ea69118508e0fa269
emmiiorji <emmanuelorji2013@gmail.com>
2022-05-26 18:52:23 +0100

    Create popup for reservation

--- src/modules/reservation.controller.js
+++ src/modules/reservation.controller.js
@@ -1,24 +1,61 @@
-// import DEFAULT from '../../config/default';
+import DEFAULT from '../../config/default.js';
 
-const renderReservationPopup = (e) => {
+const closeModal = () => {
+  document.querySelector('body').removeChild(document.querySelector('.popup-container'));
+};
+
+const createReservationsMarkup = (reservations) => {
+  let markup = '';
+  reservations.forEach((reservation, index) => {
+    markup += `${reservation.date_start} - ${reservation.date_end}`;
+    markup += (index < reservations.length - 1) ? '<br>' : '';
+  });
+  return markup;
+};
+
+const renderReservationPopup = async (e) => {
   const movieId = parseInt(e.target.id.split('_')[1], 10);
-  const tooltip = e.target.parentElement.parentElement;
-  const imgURL = tooltip.querySelector('.movie-img').src;
-  const label = tooltip.querySelector('label').innerText;
+  let response = await fetch(`${DEFAULT.MOVE_API_URL}/${movieId}`);
+  const movie = await response.json();
+  response = await fetch(`${DEFAULT.INVOLVEMENT_API_BASEURL}/reservations?item_id=${movieId}`);
+  const reservations = response.ok ? await response.json() : [];
 
   const reservePopup = document.createElement('div');
   reservePopup.classList.add('popup-container');
 
   reservePopup.innerHTML = `
-                          <div class="reserve-popup">
-                          <img src=${imgURL} alt="" class="movie-img  "/>
-                          <Label>${label}</Label>
+                            <div class="reserve-popup">
+                              <div class="item-details">
+                                <img src=${movie.image.medium} alt="" class="movie-img  "/>
+                                <span class="material-symbols-outlined close">close</span>
+                                <h2>${movie.name}</h2>
+                                <ul>
+                                  <li><span class="caption">Premiered:</span> ${movie.premiered}</li>
+                                  <li><span class="caption">Rating:</span> ${movie.rating.average}</li>
+                                  <li><span class="caption">Runtime:</span> ${movie.averageRuntime}</li>
+                                  <li><span class="caption">Genres:</span> ${movie.genres.join(', ')}</li>
+                                </ul>
+                              </div>
+                              
+                              <div class="reservations">
+                                <h2>Reservations(${reservations.length})</h2>
+                                ${createReservationsMarkup(reservations) || 'This movie has no reservation yet'}
+                              </div>
 
-                          <p>Reservations</p>
-                          </div>
+                              <form class="add-reservation">
+                                <h2>Add a reservation</h2>
+                                <input type="text" placeholder="Enter your name" required>
+                                <input type="date" required>
+                                <input type="date" required>
+                                <input type="submit" value="Reserve">
+                              </form>
+                            </div>
+                          
                           `;
 
   document.querySelector('body').appendChild(reservePopup);
+
+  document.querySelector('.material-symbols-outlined.close').addEventListener('click', closeModal);
 };
 
 // const requestReservation = async () => {
